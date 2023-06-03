f331b6d093275b9b91eec2a62bcb53f95967d350
Hermeshasnowings <67393284+Hermeshasnowings@users.noreply.github.com>
2023-06-03 17:57:55 +0100

    Revert "first commit"
    This reverts commit d901d61805b50bd08db4d518111b5e6f631bae88.

--- database.php
+++ database.php
@@ -3,4 +3,11 @@ $dbname = ''; // name of the database
 $user = 'admin'; // my login
 $password = 'Integraftw8484';
  // password for login
+
+ try {
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    exit; // Exit the script if there's an error
+}
 ?>
